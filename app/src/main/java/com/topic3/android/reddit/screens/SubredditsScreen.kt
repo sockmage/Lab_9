87c3f3c1e4c210867013894c411dec808051027c
sockmage <cockmage@icloud.com>
2024-06-02 01:22:12 +0300

    Добавление списка сообщества

--- app/src/main/java/com/topic3/android/reddit/screens/SubredditsScreen.kt
+++ app/src/main/java/com/topic3/android/reddit/screens/SubredditsScreen.kt
@@ -29,6 +29,7 @@ import androidx.compose.ui.unit.sp
 import androidx.compose.ui.zIndex
 import androidx.constraintlayout.compose.ConstraintLayout
 import com.topic3.android.reddit.R
+import com.topic3.android.reddit.components.BackgroundText
 import com.topic3.android.reddit.models.SubredditModel
 
 val subreddits = listOf(
@@ -221,7 +222,14 @@ fun Community(
 
 @Composable
 fun Communities(modifier: Modifier = Modifier) {
-    //TODO add your code here
+    mainCommunities.forEach {
+        Community(text = stringResource(it))
+    }
+    Spacer(modifier = modifier.height(4.dp))
+    BackgroundText(stringResource(R.string.communities))
+    communities.forEach {
+        Community(text = stringResource(it))
+    }
 }
 
 @Preview
